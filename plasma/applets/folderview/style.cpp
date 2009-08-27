138f1445c866cfa702356ef792f652d4b37e43c9
Fredrik Höglund <fredrik@kde.org>
2009-08-27 00:06:39 +0000

    Backport r1016088.
    svn path=/branches/KDE/4.3/kdebase/apps/; revision=1016089

--- plasma/applets/folderview/style.cpp
+++ plasma/applets/folderview/style.cpp
@@ -1,5 +1,5 @@
 /*
- *   Copyright © 2008 Fredrik Höglund <fredrik@kde.org>
+ *   Copyright © 2008, 2009 Fredrik Höglund <fredrik@kde.org>
  *
  *   This library is free software; you can redistribute it and/or
  *   modify it under the terms of the GNU Library General Public
@@ -19,6 +19,7 @@
 
 #include "style.h"
 
+#include <QPainter>
 #include <QStyleOption>
 
 #include <plasma/framesvg.h>
@@ -39,26 +40,62 @@ FolderViewStyle::~FolderViewStyle()
 void FolderViewStyle::drawPrimitive(PrimitiveElement element, const QStyleOption *option,
                                     QPainter *painter, const QWidget *widget) const
 {
-    if (element != PE_PanelItemViewItem) {
+    switch (element)
+    {
+    case PE_PanelItemViewItem:
+    {
+        const bool hover = (option->state & State_MouseOver);
+        const bool selected = (option->state & State_Selected);
+
+        if (selected && hover) {
+            m_frame->setElementPrefix("selected+hover");
+        } else if (selected) {
+            m_frame->setElementPrefix("selected");
+        } else if (hover) {
+            m_frame->setElementPrefix("hover");
+        } else {
+            m_frame->setElementPrefix("normal");
+        }
+    
+        if (selected || hover) {
+            m_frame->resizeFrame(option->rect.size());
+            m_frame->paintFrame(painter, option->rect.topLeft());
+        }
+        break;
+    }
+
+    case PE_FrameFocusRect:
+    {
+        //QColor color = option->palette.color(QPalette::Text);
+        QColor color = Qt::white;
+        color.setAlphaF(.33);
+
+        QColor transparent = color;
+        transparent.setAlphaF(0);
+
+        QLinearGradient g1(0, option->rect.top(), 0, option->rect.bottom());
+        g1.setColorAt(0, color);
+        g1.setColorAt(1, transparent);
+
+        QLinearGradient g2(option->rect.left(), 0, option->rect.right(), 0);
+        g2.setColorAt(0, transparent);
+        g2.setColorAt(.5, color);
+        g2.setColorAt(1, transparent);
+
+        painter->save();
+        painter->setRenderHint(QPainter::Antialiasing);
+        painter->setPen(QPen(g1, 0));
+        painter->setBrush(Qt::NoBrush);
+        painter->drawRoundedRect(QRectF(option->rect).adjusted(.5, .5, -.5, -.5), 2, 2);
+        painter->setPen(QPen(g2, 0));
+        painter->drawLine(QLineF(option->rect.left() + 2, option->rect.bottom() + .5,
+                                 option->rect.right() - 2, option->rect.bottom() + .5));
+        painter->restore();
+        break;
+    }
+
+    default:
         return QCommonStyle::drawPrimitive(element, option, painter, widget);
-    }
-
-    const bool hover = (option->state & State_MouseOver);
-    const bool selected = (option->state & State_Selected);
-
-    if (selected && hover) {
-        m_frame->setElementPrefix("selected+hover");
-    } else if (selected) {
-        m_frame->setElementPrefix("selected");
-    } else if (hover) {
-        m_frame->setElementPrefix("hover");
-    } else {
-        m_frame->setElementPrefix("normal");
-    }
-
-    if (selected || hover) {
-        m_frame->resizeFrame(option->rect.size());
-        m_frame->paintFrame(painter, option->rect.topLeft());
     }
 }
 
