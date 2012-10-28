KDE/kde-baseapps

commit 429218eaf22c1bde24ba6875895c33facfa6ae12

Implemented multithreading in KFileItemModelSortAlgorithm. Use as many threads as possible. Thanks to Frank Reininghaus for all his suggestions.
REVIEW: 107025

dolphin/src/kitemviews/private/kfileitemmodelsortalgorithm.cpp
@@ -19,9 +19,20 @@
 
 #include "kfileitemmodelsortalgorithm.h"
 
+#include <QThread>
+#include <QtCore>
+
 void KFileItemModelSortAlgorithm::sort(KFileItemModel* model,
                                        QList<KFileItemModel::ItemData*>::iterator begin,
                                        QList<KFileItemModel::ItemData*>::iterator end)
+{
+    static const int numberOfThreads = QThread::idealThreadCount();
+    parallelSort(model, begin, end, numberOfThreads);
+}
+
+void KFileItemModelSortAlgorithm::sequentialSort(KFileItemModel* model,
+                                                 QList< KFileItemModel::ItemData* >::iterator begin,
+                                                 QList< KFileItemModel::ItemData* >::iterator end)
 {
     // The implementation is based on qStableSortHelper() from qalgorithms.h
     // Copyright (C) 2011 Nokia Corporation and/or its subsidiary(-ies).
@@ -35,6 +46,28 @@ void KFileItemModelSortAlgorithm::sort(KFileItemModel* model,
     sort(model, begin, middle);
     sort(model, middle, end);
     merge(model, begin, middle, end);
+}
+
+void KFileItemModelSortAlgorithm::parallelSort(KFileItemModel* model,
+                                               QList< KFileItemModel::ItemData* >::iterator begin,
+                                               QList< KFileItemModel::ItemData* >::iterator end,
+                                               const int numberOfThreads)
+{
+    const int span = end - begin;
+
+    if (numberOfThreads > 1 && span > 100) {
+        const int newNumberOfThreads = numberOfThreads / 2;
+        const QList<KFileItemModel::ItemData*>::iterator middle = begin + span / 2;
+
+        QFuture<void> future = QtConcurrent::run(parallelSort, model, begin, middle, newNumberOfThreads);
+        parallelSort(model, middle, end, newNumberOfThreads);
+
+        future.waitForFinished();
+
+        merge(model, begin, middle, end);
+    } else {
+        sequentialSort(model, begin, end);
+    }
 }
 
 void KFileItemModelSortAlgorithm::merge(KFileItemModel* model,

dolphin/src/kitemviews/private/kfileitemmodelsortalgorithm.h
@@ -44,6 +44,15 @@ public:
                      QList<KFileItemModel::ItemData*>::iterator end);
 
 private:
+    static void sequentialSort(KFileItemModel* model,
+                               QList<KFileItemModel::ItemData*>::iterator begin,
+                               QList<KFileItemModel::ItemData*>::iterator end);
+
+    static void parallelSort(KFileItemModel* model,
+                             QList<KFileItemModel::ItemData*>::iterator begin,
+                             QList<KFileItemModel::ItemData*>::iterator end,
+                             const int numberOfThreads);
+
     static void merge(KFileItemModel* model,
                       QList<KFileItemModel::ItemData*>::iterator begin,
                       QList<KFileItemModel::ItemData*>::iterator pivot,

commit 2e9e2ecc6e85a0540404c23c84f39b0e09431d1d

Fix wrong function call in KFileItemModelSortAlgorithm::sequentialSort (Bug introduced in commit 429218eaf22c1bde24ba6875895c33facfa6ae12)

dolphin/src/kitemviews/private/kfileitemmodelsortalgorithm.cpp
@@ -43,8 +43,8 @@ void KFileItemModelSortAlgorithm::sequentialSort(KFileItemModel* model,
     }
 
     const QList<KFileItemModel::ItemData*>::iterator middle = begin + span / 2;
-    sort(model, begin, middle);
-    sort(model, middle, end);
+    sequentialSort(model, begin, middle);
+    sequentialSort(model, middle, end);
     merge(model, begin, middle, end);
 }
 