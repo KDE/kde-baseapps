2150e638381e13fd3fd1202320d344556b09d585
Ignat Semenov <ignat.semenov@blue-systems.com>
2012-11-23 15:02:49 +0400

    start of a separate settings object

---
+++ plasma/applets/folderview/settings/settings.h
@@ -0,0 +1,65 @@
+/*
+ *   Copyright © 2012 Ignat Semenov <ignat.semenov@blue-systems.com>
+ *
+ *   This library is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU Library General Public
+ *   License as published by the Free Software Foundation; either
+ *   version 2 of the License, or (at your option) any later version.
+ *
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *   Library General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Library General Public License
+ *   along with this library; see the file COPYING.LIB.  If not, write to
+ *   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ *   Boston, MA 02110-1301, USA.
+ */
+
+#ifndef SETTINGS_H
+#define SETTINGS_H
+
+#include <QObject>
+#include <QColor>
+#include <QStringList>
+
+#include <KUrl>
+
+#include "../iconview.h"
+
+class Settings : public QObject
+{
+public:
+
+    Settings();
+
+private:
+
+    KUrl m_url;
+
+    QColor m_textColor;
+    QString m_titleText;
+    int m_sortColumn;
+    Qt::SortOrder m_sortOrder;
+    bool m_sortDirsFirst;
+    bool m_showPreviews;
+    bool m_drawShadows;
+    bool m_iconsLocked;
+    bool m_alignToGrid;
+    bool m_clickToView;
+    bool m_showSelectionMarker;
+    bool m_userSelectedShowAllFiles;
+    bool m_blankLabel;
+    QString m_customLabel;
+    QStringList m_previewPlugins;
+    int m_customIconSize;
+    int m_numTextLines;
+    IconView::Flow m_flow;
+
+    int m_filterType;
+    QString m_filterFiles;
+    QStringList m_filterFilesMimeList;
+};
+
+#endif
