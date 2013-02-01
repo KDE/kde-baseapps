07402f892071ab63823dd46f00756ef8be7a0e2d
Aaron Seigo <aseigo@kde.org>
2013-02-01 21:54:29 +0100

    automatically focus the line edit when custom title is selected

--- plasma/applets/folderview/folderview.cpp
+++ plasma/applets/folderview/folderview.cpp
@@ -1950,6 +1950,7 @@ void FolderView::setTitleEditEnabled(int index)
 {
     if (uiLocation.titleCombo->itemData(index).toInt() == FolderView::Custom) {
         uiLocation.titleEdit->setEnabled(true);
+        uiLocation.titleEdit->setFocus();
     } else {
         uiLocation.titleEdit->setEnabled(false);
     }
