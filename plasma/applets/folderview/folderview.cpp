f9728d323e8c78a18fbb37fe45bc4eb0cdec0fe1
Ignat Semenov <ignat.semenov@blue-systems.com>
2013-02-02 20:17:36 +0400

    style

--- plasma/applets/folderview/folderview.cpp
+++ plasma/applets/folderview/folderview.cpp
@@ -1919,12 +1919,11 @@ QSizeF FolderView::sizeHint(Qt::SizeHint which, const QSizeF &constraint) const
               }
               break;
           case Plasma::Horizontal:
-          case Plasma::Vertical:
-                {
-                    const int iconSize = IconSize(KIconLoader::Panel);
-                    size = QSizeF(iconSize, iconSize);
-                    break;
-                }
+          case Plasma::Vertical: {
+              const int iconSize = IconSize(KIconLoader::Panel);
+              size = QSizeF(iconSize, iconSize);
+              break;
+          }
           default:
               break;
         }
