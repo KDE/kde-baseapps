378c4476abdc86d2dbdcbadfd3dc24a63e881aaf
Eike Hein <hein@kde.org>
2013-05-23 10:03:02 +0200

    Cleanup.
    Abusing the saved positions system to cope with theme changes
    is no longer necessary now that updateGridSize() no longer
    throws out position data.

--- plasma/applets/folderview/iconview.cpp
+++ plasma/applets/folderview/iconview.cpp
@@ -1700,9 +1700,6 @@ void IconView::checkIfFolderResult(const QModelIndex &index, bool isFolder)
 
 void IconView::svgChanged()
 {
-    setIconPositionsData(iconPositionsData());
-    m_delayedCacheClearTimer.start(5000, this);
-
     for (int i = 0; i < m_validRows; ++i) {
         m_items[i].needSizeAdjust = true;
     }
