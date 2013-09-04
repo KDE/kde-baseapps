KDE/kde-baseapps

commit d1963e23ddfd64c4ffb18da419dc9c3f92300341

Do not crash while importing/adding bookmarks.
BUG: 309370
REVIEW: 112042
FIXED-IN: 4.11.1

konqueror/src/konqbookmarkbar.cpp
@@ -185,11 +185,15 @@ void KBookmarkBar::fillBookmarkBar(const KBookmarkGroup & parent)
         if (!bm.isGroup())
         {
 	    if ( bm.isSeparator() )
-                m_toolBar->addSeparator();
+                if (m_toolBar) {
+                    m_toolBar->addSeparator();
+                }
             else
             {
                 KAction *action = new KBookmarkAction( bm, m_pOwner, 0 );
-                m_toolBar->addAction(action);
+                if (m_toolBar) {
+                    m_toolBar->addAction(action);
+                }
                 d->m_actions.append( action );
             }
         }
@@ -197,7 +201,9 @@ void KBookmarkBar::fillBookmarkBar(const KBookmarkGroup & parent)
         {
             KBookmarkActionMenu *action = new KBookmarkActionMenu(bm, 0);
             action->setDelayed( false );
-            m_toolBar->addAction(action);
+            if (m_toolBar) {
+                m_toolBar->addAction(action);
+            }
             d->m_actions.append( action );
             KBookmarkMenu *menu = new KonqBookmarkMenu(m_pManager, m_pOwner, action, bm.address());
             m_lstSubMenus.append( menu );

commit 43a981481ab9161a83dd200db39c093e2502f68a

Fix toplevel bookmarks not showing in toolbar regression from d1963e23

konqueror/src/konqbookmarkbar.cpp
@@ -184,10 +184,12 @@ void KBookmarkBar::fillBookmarkBar(const KBookmarkGroup & parent)
 
         if (!bm.isGroup())
         {
-	    if ( bm.isSeparator() )
+            if ( bm.isSeparator() )
+            {
                 if (m_toolBar) {
                     m_toolBar->addSeparator();
                 }
+            }
             else
             {
                 KAction *action = new KBookmarkAction( bm, m_pOwner, 0 );