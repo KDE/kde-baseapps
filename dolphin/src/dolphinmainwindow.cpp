KDE/kde-baseapps

commit 6a98d83312ff2b14ab878e1427bbace3b683eacc

Implemented DolphinTabPage class to encapsulate the split view handling from DolphinMainWindow.
The signal activeViewUrlChanged in DolphinTabPage is currently unused, but we need it later when
we implement the tab widget and tab bar.

DolphinTabPage has saveState/restoreState which are using a QByteArray instead of the KConfigGroup to
be more generic.

REVIEW: 118964

dolphin/src/dolphintabpage.cpp
@@ -1,0 +1,258 @@
+/***************************************************************************
+ * Copyright (C) 2014 by Emmanuel Pescosta <emmanuelpescosta099@gmail.com> *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA            *
+ ***************************************************************************/
+
+#include "dolphintabpage.h"
+
+#include "dolphinviewcontainer.h"
+#include "dolphin_generalsettings.h"
+
+#include <QSplitter>
+
+DolphinTabPage::DolphinTabPage(const KUrl& primaryUrl, const KUrl& secondaryUrl, QWidget* parent) :
+    QWidget(parent),
+    m_primaryViewActive(true),
+    m_splitViewEnabled(false)
+{
+    QVBoxLayout* layout = new QVBoxLayout(this);
+    layout->setSpacing(0);
+    layout->setMargin(0);
+
+    m_splitter = new QSplitter(Qt::Horizontal, this);
+    m_splitter->setChildrenCollapsible(false);
+    layout->addWidget(m_splitter);
+
+    // Create a new primary view
+    m_primaryViewContainer = createViewContainer(primaryUrl);
+    connect(m_primaryViewContainer->view(), SIGNAL(urlChanged(KUrl)),
+            this, SIGNAL(activeViewUrlChanged(KUrl)));
+
+    m_splitter->addWidget(m_primaryViewContainer);
+    m_primaryViewContainer->show();
+
+    if (secondaryUrl.isValid() || GeneralSettings::splitView()) {
+        // Provide a secondary view, if the given secondary url is valid or if the
+        // startup settings are set this way (use the url of the primary view).
+        m_splitViewEnabled = true;
+        const KUrl& url = secondaryUrl.isValid() ? secondaryUrl : primaryUrl;
+        m_secondaryViewContainer = createViewContainer(url);
+        m_splitter->addWidget(m_secondaryViewContainer);
+        m_secondaryViewContainer->show();
+    }
+
+    m_primaryViewContainer->setActive(true);
+}
+
+bool DolphinTabPage::primaryViewActive() const
+{
+    return m_primaryViewActive;
+}
+
+bool DolphinTabPage::splitViewEnabled() const
+{
+    return m_splitViewEnabled;
+}
+
+void DolphinTabPage::setSplitViewEnabled(bool enabled)
+{
+    if (m_splitViewEnabled != enabled) {
+        m_splitViewEnabled = enabled;
+
+        if (enabled) {
+            const KUrl& url = m_primaryViewContainer->url();
+            m_secondaryViewContainer = createViewContainer(url);
+            m_splitter->addWidget(m_secondaryViewContainer);
+            m_secondaryViewContainer->show();
+            m_secondaryViewContainer->setActive(true);
+        } else {
+            // Close the view which is active.
+            DolphinViewContainer* view = activeViewContainer();
+            if (m_primaryViewActive) {
+                // If the primary view is active, we have to swap the pointers
+                // because the secondary view will be the new primary view.
+                qSwap(m_primaryViewContainer, m_secondaryViewContainer);
+            }
+            m_primaryViewContainer->setActive(true);
+            view->close();
+            view->deleteLater();
+        }
+    }
+}
+
+DolphinViewContainer* DolphinTabPage::primaryViewContainer() const
+{
+    return m_primaryViewContainer;
+}
+
+DolphinViewContainer* DolphinTabPage::secondaryViewContainer() const
+{
+    return m_secondaryViewContainer;
+}
+
+DolphinViewContainer* DolphinTabPage::activeViewContainer() const
+{
+    return m_primaryViewActive ? m_primaryViewContainer :
+                                 m_secondaryViewContainer;
+}
+
+KFileItemList DolphinTabPage::selectedItems() const
+{
+    KFileItemList items = m_primaryViewContainer->view()->selectedItems();
+    if (m_splitViewEnabled) {
+        items += m_secondaryViewContainer->view()->selectedItems();
+    }
+    return items;
+}
+
+int DolphinTabPage::selectedItemsCount() const
+{
+    int selectedItemsCount = m_primaryViewContainer->view()->selectedItemsCount();
+    if (m_splitViewEnabled) {
+        selectedItemsCount += m_secondaryViewContainer->view()->selectedItemsCount();
+    }
+    return selectedItemsCount;
+}
+
+void DolphinTabPage::markUrlsAsSelected(const QList<KUrl>& urls)
+{
+    m_primaryViewContainer->view()->markUrlsAsSelected(urls);
+    if (m_splitViewEnabled) {
+        m_secondaryViewContainer->view()->markUrlsAsSelected(urls);
+    }
+}
+
+void DolphinTabPage::markUrlAsCurrent(const KUrl& url)
+{
+    m_primaryViewContainer->view()->markUrlAsCurrent(url);
+    if (m_splitViewEnabled) {
+        m_secondaryViewContainer->view()->markUrlAsCurrent(url);
+    }
+}
+
+void DolphinTabPage::setPlacesSelectorVisible(bool visible)
+{
+    m_primaryViewContainer->urlNavigator()->setPlacesSelectorVisible(visible);
+    if (m_splitViewEnabled) {
+        m_secondaryViewContainer->urlNavigator()->setPlacesSelectorVisible(visible);
+    }
+}
+
+void DolphinTabPage::refreshViews()
+{
+    m_primaryViewContainer->readSettings();
+    if (m_splitViewEnabled) {
+        m_secondaryViewContainer->readSettings();
+    }
+}
+
+QByteArray DolphinTabPage::saveState() const
+{
+    QByteArray state;
+    QDataStream stream(&state, QIODevice::WriteOnly);
+
+    stream << m_splitViewEnabled;
+
+    stream << m_primaryViewContainer->url();
+    stream << m_primaryViewContainer->urlNavigator()->isUrlEditable();
+
+    if (m_splitViewEnabled) {
+        stream << m_secondaryViewContainer->url();
+        stream << m_secondaryViewContainer->urlNavigator()->isUrlEditable();
+    }
+
+    stream << m_primaryViewActive;
+    stream << m_splitter->saveState();
+
+    return state;
+}
+
+void DolphinTabPage::restoreState(const QByteArray& state)
+{
+    QByteArray sd = state;
+    QDataStream stream(&sd, QIODevice::ReadOnly);
+
+    bool isSplitViewEnabled = false;
+    stream >> isSplitViewEnabled;
+    setSplitViewEnabled(isSplitViewEnabled);
+
+    KUrl primaryUrl;
+    stream >> primaryUrl;
+    m_primaryViewContainer->setUrl(primaryUrl);
+    bool primaryUrlEditable;
+    stream >> primaryUrlEditable;
+    m_primaryViewContainer->urlNavigator()->setUrlEditable(primaryUrlEditable);
+
+    if (isSplitViewEnabled) {
+        KUrl secondaryUrl;
+        stream >> secondaryUrl;
+        m_secondaryViewContainer->setUrl(secondaryUrl);
+        bool secondaryUrlEditable;
+        stream >> secondaryUrlEditable;
+        m_secondaryViewContainer->urlNavigator()->setUrlEditable(secondaryUrlEditable);
+    }
+
+    stream >> m_primaryViewActive;
+    if (m_primaryViewActive) {
+        m_primaryViewContainer->setActive(true);
+    } else {
+        Q_ASSERT(m_splitViewEnabled);
+        m_secondaryViewContainer->setActive(true);
+    }
+
+    QByteArray splitterState;
+    stream >> splitterState;
+    m_splitter->restoreState(splitterState);
+}
+
+void DolphinTabPage::slotViewActivated()
+{
+    const DolphinView* oldActiveView = activeViewContainer()->view();
+
+    // Set the view, which was active before, to inactive
+    // and update the active view type.
+    if (m_splitViewEnabled) {
+        activeViewContainer()->setActive(false);
+        m_primaryViewActive = !m_primaryViewActive;
+    } else {
+        m_primaryViewActive = true;
+    }
+
+    const DolphinView* newActiveView = activeViewContainer()->view();
+
+    if (newActiveView != oldActiveView) {
+        disconnect(oldActiveView, SIGNAL(urlChanged(KUrl)),
+                   this, SIGNAL(activeViewUrlChanged(KUrl)));
+        connect(newActiveView, SIGNAL(urlChanged(KUrl)),
+                this, SIGNAL(activeViewUrlChanged(KUrl)));
+    }
+
+    emit activeViewUrlChanged(activeViewContainer()->url());
+    emit activeViewChanged();
+}
+
+DolphinViewContainer* DolphinTabPage::createViewContainer(const KUrl& url) const
+{
+    DolphinViewContainer* container = new DolphinViewContainer(url, m_splitter);
+    container->setActive(false);
+
+    const DolphinView* view = container->view();
+    connect(view, SIGNAL(activated()),
+            this, SLOT(slotViewActivated()));
+
+    return container;
+}

dolphin/src/CMakeLists.txt
@@ -172,6 +172,7 @@ set(dolphin_SRCS
     dolphinviewcontainer.cpp
     dolphincontextmenu.cpp
     dolphinrecenttabsmenu.cpp
+    dolphintabpage.cpp
     filterbar/filterbar.cpp
     main.cpp
     panels/information/filemetadataconfigurationdialog.cpp

dolphin/src/dolphinmainwindow.h
@@ -40,6 +40,7 @@ class DolphinApplication;
 class DolphinSettingsDialog;
 class DolphinViewContainer;
 class DolphinRemoteEncoding;
+class DolphinTabPage;
 class KAction;
 class KFileItem;
 class KFileItemList;
@@ -87,13 +88,6 @@ public:
      * of Dolphin).
      */
     void openFiles(const QList<KUrl>& files);
-
-    /**
-     * Returns true, if the main window contains two instances
-     * of a view container. The active view constainer can be
-     * accessed by DolphinMainWindow::activeViewContainer().
-     */
-    bool isSplit() const;
 
     /**
      * Returns the 'Create New...' sub menu which also can be shared
@@ -353,15 +347,16 @@ private slots:
     void openNewTab();
 
     /**
-     * Opens a new tab in the background showing the URL \a url.
+     * Opens a new tab in the background showing the URL \a primaryUrl and the
+     * optional URL \a secondaryUrl.
      */
-    void openNewTab(const KUrl& url);
+    void openNewTab(const KUrl& primaryUrl, const KUrl& secondaryUrl = KUrl());
 
     /**
-     * Opens a new tab showing the URL \a url and activates
-     * the tab.
+     * Opens a new tab showing the  URL \a primaryUrl and the optional URL
+     * \a secondaryUrl and activates the tab.
      */
-    void openNewActivatedTab(const KUrl& url);
+    void openNewActivatedTab(const KUrl& primaryUrl, const KUrl& secondaryUrl = KUrl());
 
     void activateNextTab();
 
@@ -376,9 +371,6 @@ private slots:
      * Opens the selected folder in a new window.
      */
     void openInNewWindow();
-
-    /** Toggles the active view if two views are shown within the main window. */
-    void toggleActiveView();
 
     /**
      * Indicates in the statusbar that the execution of the command \a command
@@ -474,11 +466,7 @@ private slots:
      */
     void slotPlaceActivated(const KUrl& url);
 
-    /**
-     * Is called when the user wants to reopen a previously closed \a tab from
-     * the recent tabs menu.
-     */
-    void restoreClosedTab(const KUrl& primaryUrl, const KUrl& secondaryUrl);
+    void activeViewChanged();
 
 private:
     /**
@@ -488,11 +476,6 @@ private:
      * is usually shown in darker colors.
      */
     void setActiveViewContainer(DolphinViewContainer* view);
-
-    /**
-     * Creates a view container and does a default initialization.
-     */
-    DolphinViewContainer* createViewContainer(const KUrl& url, QWidget* parent);
 
     void setupActions();
     void setupDockWidgets();
@@ -529,16 +512,8 @@ private:
     /** Returns the name of the tab for the URL \a url. */
     QString tabName(const KUrl& url) const;
 
+
     bool isKompareInstalled() const;
-
-    void createSecondaryView(int tabIndex);
-
-    /**
-     * Helper method for saveProperties() and readProperties(): Returns
-     * the property string for a tab with the index \a tabIndex and
-     * the property \a property.
-     */
-    QString tabProperty(const QString& property, int tabIndex) const;
 
     /**
      * Sets the window caption to url.fileName() if this is non-empty,
@@ -579,18 +554,8 @@ private:
     QVBoxLayout* m_centralWidgetLayout;
     int m_id;
 
-    // Members for the tab-handling:
-    struct ViewTab
-    {
-        ViewTab() : isPrimaryViewActive(true), wasActive(false), primaryView(0), secondaryView(0), splitter(0) {}
-        bool isPrimaryViewActive;
-        bool wasActive;
-        DolphinViewContainer* primaryView;
-        DolphinViewContainer* secondaryView;
-        QSplitter* splitter;
-    };
     int m_tabIndex;
-    QList<ViewTab> m_viewTab;
+    QList<DolphinTabPage*> m_viewTab;
 
     DolphinViewActionHandler* m_actionHandler;
     DolphinRemoteEncoding* m_remoteEncoding;
@@ -606,11 +571,6 @@ private:
 inline DolphinViewContainer* DolphinMainWindow::activeViewContainer() const
 {
     return m_activeViewContainer;
-}
-
-inline bool DolphinMainWindow::isSplit() const
-{
-    return m_viewTab[m_tabIndex].secondaryView != 0;
 }
 
 inline KNewFileMenu* DolphinMainWindow::newFileMenu() const

dolphin/src/dolphintabpage.h
@@ -1,0 +1,152 @@
+/***************************************************************************
+ * Copyright (C) 2014 by Emmanuel Pescosta <emmanuelpescosta099@gmail.com> *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA            *
+ ***************************************************************************/
+
+#ifndef DOLPHIN_TAB_PAGE_H
+#define DOLPHIN_TAB_PAGE_H
+
+#include <QWidget>
+#include <QPointer>
+#include <KUrl>
+
+class QSplitter;
+class DolphinViewContainer;
+class KFileItemList;
+
+class DolphinTabPage : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit DolphinTabPage(const KUrl& primaryUrl, const KUrl& secondaryUrl = KUrl(), QWidget* parent = 0);
+
+    /**
+     * @return True if primary view is the active view in this tab.
+     */
+    bool primaryViewActive() const;
+
+    /**
+     * @return True if split view is enabled.
+     */
+    bool splitViewEnabled() const;
+
+    /**
+     * Enables or disables the split view mode.
+     *
+     * If \a enabled is true, it creates a secondary view with the url of the primary view.
+     */
+    void setSplitViewEnabled(bool enabled);
+
+    /**
+     * @return The primary view containter.
+     */
+    DolphinViewContainer* primaryViewContainer() const;
+
+    /**
+     * @return The secondary view containter, can be 0 if split view is disabled.
+     */
+    DolphinViewContainer* secondaryViewContainer() const;
+
+    /**
+     * @return DolphinViewContainer of the active view
+     */
+    DolphinViewContainer* activeViewContainer() const;
+
+    /**
+     * Returns the selected items. The list is empty if no item has been
+     * selected.
+     */
+    KFileItemList selectedItems() const;
+
+    /**
+     * Returns the number of selected items (this is faster than
+     * invoking selectedItems().count()).
+     */
+    int selectedItemsCount() const;
+
+    /**
+     * Marks the items indicated by \p urls to get selected after the
+     * directory DolphinView::url() has been loaded. Note that nothing
+     * gets selected if no loading of a directory has been triggered
+     * by DolphinView::setUrl() or DolphinView::reload().
+     */
+    void markUrlsAsSelected(const QList<KUrl>& urls);
+
+    /**
+     * Marks the item indicated by \p url to be scrolled to and as the
+     * current item after directory DolphinView::url() has been loaded.
+     */
+    void markUrlAsCurrent(const KUrl& url);
+
+    /**
+     * Sets the places selector visible, if \a visible is true.
+     * The places selector allows to select the places provided
+     * by the places model passed in the constructor. Per default
+     * the places selector is visible.
+     */
+    void setPlacesSelectorVisible(bool visible);
+
+    /**
+     * Refreshes the views of the main window by recreating them according to
+     * the given Dolphin settings.
+     */
+    void refreshViews();
+
+    /**
+     * Saves all tab related properties (urls, splitter layout, ...).
+     *
+     * @return A byte-array which contains all properties.
+     */
+    QByteArray saveState() const;
+
+    /**
+     * Restores all tab related properties (urls, splitter layout, ...) from
+     * the given \a state.
+     */
+    void restoreState(const QByteArray& state);
+
+signals:
+    void activeViewChanged();
+    void activeViewUrlChanged(const KUrl& url);
+
+private slots:
+    /**
+     * Handles the view activated event.
+     *
+     * It sets the previous active view to inactive, updates the current
+     * active view type and triggers the activeViewChanged event.
+     */
+    void slotViewActivated();
+
+private:
+    /**
+     * Creates a new view container and does the default initialization.
+     */
+    DolphinViewContainer* createViewContainer(const KUrl& url) const;
+
+private:
+    QSplitter* m_splitter;
+
+    QPointer<DolphinViewContainer> m_primaryViewContainer;
+    QPointer<DolphinViewContainer> m_secondaryViewContainer;
+
+    bool m_primaryViewActive;
+    bool m_splitViewEnabled;
+};
+
+#endif // DOLPHIN_TAB_PAGE_H

dolphin/src/dolphinviewcontainer.cpp
@@ -125,6 +125,7 @@ DolphinViewContainer::DolphinViewContainer(const KUrl& url, QWidget* parent) :
     connect(m_view, SIGNAL(urlAboutToBeChanged(KUrl)),          this, SLOT(slotViewUrlAboutToBeChanged(KUrl)));
     connect(m_view, SIGNAL(errorMessage(QString)),              this, SLOT(showErrorMessage(QString)));
     connect(m_view, SIGNAL(urlIsFileError(KUrl)),               this, SLOT(slotUrlIsFileError(KUrl)));
+    connect(m_view, SIGNAL(activated()),                        this, SLOT(activate()));
 
     connect(m_urlNavigator, SIGNAL(urlAboutToBeChanged(KUrl)),
             this, SLOT(slotUrlNavigatorLocationAboutToBeChanged(KUrl)));

commit 15e68c7a02bee0c2c7ee36228ab5043544e0f428

Fix regression concerning the "Places" selector in the location bar
This commit ensures that the selector is visible in all location bars
if and only if the Places Panel is hidden. It fixes a regression caused
by commit 6a98d83312ff2b14ab878e1427bbace3b683eacc.

BUG: 337389
FIXED-IN: 4.14.0
REVIEW: 119238

dolphin/src/dolphinmainwindow.cpp
@@ -770,7 +770,8 @@ void DolphinMainWindow::togglePanelLockState()
 void DolphinMainWindow::slotPlacesPanelVisibilityChanged(bool visible)
 {
     foreach (DolphinTabPage* tabPage, m_viewTab) {
-        tabPage->setPlacesSelectorVisible(visible);
+        // The Places selector in the location bar should be shown if and only if the Places panel is hidden.
+        tabPage->setPlacesSelectorVisible(!visible);
     }
 }
 

dolphin/src/dolphintabpage.cpp
@@ -76,6 +76,10 @@ void DolphinTabPage::setSplitViewEnabled(bool enabled)
         if (enabled) {
             const KUrl& url = m_primaryViewContainer->url();
             m_secondaryViewContainer = createViewContainer(url);
+
+            const bool placesSelectorVisible = m_primaryViewContainer->urlNavigator()->isPlacesSelectorVisible();
+            m_secondaryViewContainer->urlNavigator()->setPlacesSelectorVisible(placesSelectorVisible);
+
             m_splitter->addWidget(m_secondaryViewContainer);
             m_secondaryViewContainer->show();
             m_secondaryViewContainer->setActive(true);